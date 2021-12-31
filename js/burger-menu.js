7ece44884f233a9121ff0054204c48a38dcacf2e
opipi <opipi406@gmail.com>
2021-12-31 21:12:02 +0900

    burger-menu, headerの修正

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -3,7 +3,7 @@ $(function () {
         $(".burger-menu, .burger-menu__lines").toggleClass("open");
     });
 
-    $("body").on("click", ".header__nav a", function () {
+    $("body").on("click", ".header__menu a", function () {
         $(".burger-menu, .burger-menu__lines").removeClass("open");
     });
 });
